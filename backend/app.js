b7be656d6a95fac1a4346a773aea72eae7f66882
Snehasish Mandal <mandalsneh97@gmail.com>
2024-09-10 12:46:51 +0530

    Updated the Environment Variables

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const connectDatabase = require("./database/database"); // Make sure this is cor
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
